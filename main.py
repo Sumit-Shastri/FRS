1886e5132fd67426af3399cb7708fa8cf096207b
Sumit-Shastri <shastrisumit25@gmail.com>
2024-12-21 13:02:16 +0530

    Added a new function

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import numpy
 print("hello")
 data = pandas.read_csv(r"E:\Learning\Data Science\DATA\SAMPLE DATA\country.csv")
 print(data)
+
+def new():
+          print("EHlllo")
+          git = 56+89
+          print(git)
